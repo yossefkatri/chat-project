de4ddb9811a1456c58c91b4c08d215994a0b01b3
yossef <yossefkatri@gmail.com>
2021-03-19 16:27:06 +0200

    i finished the first and second mission.

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import select
 import msvcrt
+import datetime
 
 IP_SERVER = "127.0.0.1"
 PORT_SERVER = 8080
@@ -8,41 +9,48 @@ PORT_SERVER = 8080
 message_to_send = []
 user_name = ''
 
-def prints(username, content):
-    print(username+": "+content)
+
+def prints(hour, username, content):
+    print(hour+" " + username + ": " + content)
 
 
 def get_response(data):
     if msvcrt.kbhit():
         ch = msvcrt.getch().decode()
-        msvcrt.putch(ch.encode())
         data += ch
         if ch == '\r':
-            print()
-            return True, data
+            hour = datetime.datetime.now().strftime("%H:%M")
+            prints(hour, user_name, data)
+            return True, hour+data
     return False, data
 
-#TODO get name MAX 4 charcters
+
 def get_name():
     name = input("enter user-name:")
     return name
 
 
 def get_pkt(msg):
-    p = str(len(user_name)).zfill(4) + user_name + str(len(msg)) + msg
+    p = str(len(user_name)).zfill(4) + user_name + str(len(msg)).zfill(4) + msg
     return p.encode()
 
-#TODO extract the data and print 'user: msg'
-def print_pkt(data):
-    pass
+
+def print_pkt(data):# format: 0003 asd 0008 16:02 ffg
+    user_len = int(data[:4])
+    user = data[4:user_len+4]
+    len = int(data[user_len+4:user_len + 8])
+    msg = data[user_len + 8:user_len + 8 + len]
+    hour = msg[:5]
+    msg = msg[5:]
+    prints(hour, user, msg)
 
 
 def main():
     my_socket = socket.socket()
     my_socket.connect((IP_SERVER, PORT_SERVER))
     print(" welcome to the chat!!")
+    global user_name
     user_name = get_name()
-    print(user_name, end=':')
     msg = ''
     while True:
         rlist, wlist, xlist = select.select([my_socket], [my_socket], [])
@@ -50,14 +58,13 @@ def main():
             data = my_socket.recv(1024).decode()
             print_pkt(data)
         if my_socket in wlist:
-            for msg in message_to_send:
-                my_socket.send(get_pkt(msg))
-                message_to_send.remove(msg)
+            for msg1 in message_to_send:
+                my_socket.send(get_pkt(msg1))
+                message_to_send.remove(msg1)
         flag, data = get_response(msg)
         if flag:
             message_to_send.append(data)
             msg = ''
-            print(user_name, end=':')
         else:
             msg = data
     my_socket.close()
